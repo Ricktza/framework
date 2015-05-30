416c9420337b1f18d9021d9be20f40eccb927f2e
Ricktza <rpc0321@gmail.com>
2015-05-31 00:40:32 +0100

    updates + fixes
    updates + fixes

--- framework.php
+++ framework.php
@@ -1,9 +1,6 @@
 <?php
-
 ob_start();
-$start_time = microtime(true); // starts the counter
-
-class database {
+class MyDatabase {
 
     function database($DBhostname, $DBusername, $DBpassword, $DBdatabase) { //didn't think it would act as a constructor 
         global $link;
@@ -72,16 +69,18 @@ class database {
     function prepared_query($query, $array) {
         global $db;
         $sql = $db->prepare($query);
+        if(!$sql){
+            echo $db->errorInfo();
+        }
         $sql->execute($array);
         return $sql;
-    }
-
+    }  
     
     
     
 }
 
-class functions {
+class MyFunctions {
 
     //This must be excuted before anything else.
     function redirect($url) {
@@ -94,32 +93,12 @@ class functions {
         error_reporting($value);
         ini_set('display_errors', $value);
     }
-
-    function functions() {
-//$args = func_get_args(); 
-    }
-
+    
     //good for urls or salts maybe.
     function unique_id($var) {
         return strtoupper(uniqid(strtoupper($var) . '_'));
     }
 
-//    function session_set($var, $id) {
-//        $_SESSION[$id] = $var;
-//    }
-//
-//    function session_get($id) {
-//        $return = $_SESSION[$id];
-//        return $return;
-//    }
-//
-//    function session_unset($id) {
-//        unset($_SESSION[$id]);
-//    }
-//
-//    function session_kill() {
-//        session_destroy();
-//    }
     //this can be used for protection also, because it converts tags into enties which mysql is uneffected by these.
     function html($var) {
         $var = htmlentities($var);
@@ -272,10 +251,6 @@ class functions {
         return $_SERVER['REMOTE_ADDR'];
     }
 
-    function get_real_ip() {
-        
-    }
-
     function base64_encode($var) {
         return base64_encode($var);
     }
@@ -365,22 +340,6 @@ class functions {
         }
     }
 
-    function screen($screen, $command) {
-        $command = "screen -S " . $screen . " -X stuff '" . $command . "' -ne '\015'"; //may not work can't test yet
-        exec($command);
-        shell_exec($command);
-    }
-
-    function jsondb_write($location, $content) {
-        $current = file_get_contents($file);
-        file_put_contents($file, $content);
-    }
-
-    function json_read($location) {
-        $current = file_get_contents($file);
-        return $current;
-    }
-
     function upper_case($var) {
         return strtoupper($var);
     }
@@ -404,119 +363,6 @@ class functions {
             $var = '.';
         }
         return $var;
-    }
-
-    function minecraft_api($url, $port, $timeout) {
-        define('MQ_SERVER_ADDR', $url);
-        define('MQ_SERVER_PORT', $port);
-        define('MQ_TIMEOUT', $timeout);
-        include $_SERVER['DOCUMENT_ROOT'] . "/api/MinecraftQuery.class.php";
-        $Query = new MinecraftQuery();
-        try {
-            $Query->Connect(MQ_SERVER_ADDR, MQ_SERVER_PORT, MQ_TIMEOUT);
-        } catch (MinecraftQueryException $e) {
-            $Exception = $e;
-        }
-        $count = 0;
-        if (( $Players = $Query->GetPlayers() ) !== false) {
-            foreach ($Players as $Player) {
-                $count++;
-            }
-        }
-        return $count;
-    }
-
-    function replace($txt, $code) {
-
-        if ($code > 0) {
-            $txt = str_ireplace("[green]", "<font color=GREEN>", $txt);
-            $txt = str_ireplace("[red]", "<font color=RED>", $txt);
-            $txt = str_ireplace("[blue]", "<font color=BLUE>", $txt);
-            $txt = str_ireplace("[purple]", "<font color=PURPLE>", $txt);
-            $txt = str_ireplace("[yellow]", "<font color=YELLOW>", $txt);
-            $txt = str_ireplace("[gold]", "<font color=GOLD>", $txt);
-            $txt = str_ireplace("[black]", "<font color=BLACK>", $txt);
-            $txt = str_ireplace("[orange]", "<font color=orange>", $txt);
-            $txt = str_ireplace("[pink]", "<font color=pink>", $txt);
-            $txt = str_ireplace("[grey]", "<font color=grey>", $txt);
-            $txt = str_ireplace("[gray]", "<font color=grey>", $txt);
-            $txt = str_ireplace("[white]", "<font color=white>", $txt);
-            $txt = str_ireplace("[violet]", "<font color=violet>", $txt);
-            $txt = str_ireplace("[indigo]", "<font color=indigo>", $txt);
-            $txt = str_ireplace("[/color]", "</font>", $txt);
-            $txt = str_ireplace("[/colour]", "</font>", $txt);
-            $txt = str_ireplace("[b]", "<b>", $txt);
-            $txt = str_ireplace("[/b]", "</b>", $txt);
-            $txt = str_ireplace("[i]", "<i>", $txt);
-            $txt = str_ireplace("[/i]", "<i>", $txt);
-            $txt = str_ireplace(array("\r\n", "\n", "\r"), "<br>", $txt);
-            $txt = str_ireplace("[bold]", "<b>", $txt);
-            $txt = str_ireplace("[u]", "<u>", $txt);
-            $txt = str_ireplace("[/u]", "</u>", $txt);
-        }
-//chat and forums
-        if ($code > 1) {
-            //$username = $_SESSION['username'];
-            // This so happens to not work right :P
-            //$txt = str_ireplace("@$username", "<font color=black><b>$username</b></font>", $txt);
-            //
-            //$txt = str_ireplace("$username", "<font color=black><b>$username</b></font>", $txt);
-            $txt = str_ireplace(":)", "<img src='/img/emotions/smile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":=)", "<img src='/img/emotions/smile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace("=)", "<img src='/img/emotions/smile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":-)", "<img src='/img/emotions/smile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":(", "<img src='/img/emotions/sadsmile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":=(", "<img src='/img/emotions/sadsmile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace("=(", "<img src='/img/emotions/sadsmile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":-(", "<img src='/img/emotions/sadsmile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":D", "<img src='/img/emotions/bigsmile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":-D", "<img src='/img/emotions/bigsmile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":=d", "<img src='/img/emotions/bigsmile.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(";)", "<img src='/img/emotions/wink.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(";-)", "<img src='/img/emotions/wink.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":*", "<img src='/img/emotions/kiss.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":p", "<img src='/img/emotions/tongueout.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":$", "<img src='/img/emotions/blush.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":^)", "<img src='/img/emotions/wondering.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":@", "<img src='/img/emotions/angry.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(":s", "<img src='/img/emotions/worried.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace("(y)", "<img src='/img/emotions/yes.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace("(n)", "<img src='/img/emotions/no.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(">:)", "<img src='/img/emotions/evilgrin.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace(">:(", "<img src='/img/emotions/angry.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace("(poolparty)", "<img src='/img/emotions/poolparty.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace("(drunk)", "<img src='/img/emotions/drunk.gif' width='19' height='19'>", $txt);
-            $txt = str_ireplace("(thanks)", "<img src='/img/emotions/thanks.gif' width='25' height='25'>", $txt);
-            $txt = str_ireplace("[center]", "<center>", $txt);
-            $txt = str_ireplace("[/center]", "</center>", $txt);
-            $txt = str_ireplace("[italic]", "<i>", $txt);
-            $txt = str_ireplace("[underline]", "<u>", $txt);
-            $txt = str_ireplace("[br]", "</ br>", $txt);
-            $txt = str_ireplace("[CraftTM-]", "<marquee behavior='alternate' direction='right'>", $txt);
-            $txt = str_ireplace("[hr]", "<hr>", $txt);
-            $txt = str_ireplace("[xx-small]", "<font style='font-size:xx-small;'>", $txt);
-            $txt = str_ireplace("[small]", "<font style='font-size:small;'>", $txt);
-            $txt = str_ireplace("[medium]", "<font style='font-size:medium;'>", $txt);
-            $txt = str_ireplace("[large]", "<font style='font-size:large;'>", $txt);
-            $txt = str_ireplace("[xx-large]", "<font style='font-size:x-large;'>", $txt);
-            $txt = str_ireplace("[fancy]", "<font style='font-family: Allan, helvetica, arial, sans-serif;font-size: 14px;'>", $txt);
-            $txt = str_ireplace("[o]", "<font style='font-style:oblique;'>", $txt);
-            $txt = str_ireplace("[outline]", "<span style='border:1px solid green;outline:green dotted thin;background-color:yellow;opacity:0.5;filter:Alpha(opacity=50);'>", $txt);
-            $txt = str_ireplace("[highlight]", "<span style='background-color:yellow;opacity:0.5;filter:Alpha(opacity=50);'>", $txt);
-        }
-//forums only codes.
-        if ($code > 2) {
-            $txt = str_ireplace("[/youtube]", "'></iframe>", $txt);
-            $txt = str_ireplace("[youtube]", "<br /><br /><iframe width='530' height='290' frameborder='0' allowfullscreen src='http://www.youtube.com/embed/", $txt);
-            $txt = str_ireplace("[img]", "<br /><br /><img border=0 style='max-height: 1000px; max-width: 530px;' src=", $txt);
-            $txt = str_ireplace("[/img]", ">", $txt);
-
-            $txt = str_ireplace("[url=&quot;", '<a href=', $txt);
-            $txt = str_ireplace("&quot;]", " target='_blank'>", $txt);
-
-            $txt = str_ireplace("[/url]", "</a>", $txt);
-        }
-        return $txt;
     }
 
     function ago($time, $tense_show) {
@@ -543,65 +389,5 @@ class functions {
         }
         return "$difference " . $periods[$j] . " $tense";
     }
-
-    function execution_time() {
-        Global $start_time;
-        return round(microtime(true) - $start_time, 4);
-    }
-
 }
-
-if (define('APC_EXTENSION_LOADED', extension_loaded('apc') && ini_get('apc.enabled'))) {
-
-    class cache {
-
-        function store($id, $data) {
-            apc_store($id, $data, '-1'); //-1 means forever, or at least till server restart/apache restart.
-        }
-
-        function get($id) {
-            if (apc_fetch($id)) {
-                return apc_fetch($id);
-            } else {
-                return null;
-            }
-        }
-
-        function set($id, $data, $time) {
-            if (apc_fetch($id)) {
-                return apc_fetch($id);
-            } else {
-                apc_store($id, $data, $time);
-                return apc_fetch($id);
-            }
-        }
-
-        function remove($id) {
-            apc_delete($id);
-        }
-
-        function clear() {
-            apc_clear_cache();
-        }
-
-        function dump() {
-            return apc_bin_dump($files);
-        }
-
-        function add_page($filename) {
-            apc_compile_file($filename);
-        }
-
-        function remove_page($filename) {
-            apc_delete_file($filename);
-        }
-
-        function info() {
-            apc_cache_info(); //need to format this at somepoint.
-        }
-
-    }
-
-}
-ob_clean();
-?>
+ob_clean(); 
